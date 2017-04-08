24ee3e8297be7c4c5eeb8c9be730012e215d94f9
Divya Yadav <divyayadav1606@gmail.com>
2017-04-08 01:20:23 -0700

    Added floation action menu

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/ChallengeDetailsActivity.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/ChallengeDetailsActivity.java
@@ -1,15 +1,38 @@
 package com.accountabilibuddies.accountabilibuddies.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.accountabilibuddies.accountabilibuddies.R;
+import com.accountabilibuddies.accountabilibuddies.databinding.ActivityChallengeDetailsBinding;
 
 public class ChallengeDetailsActivity extends AppCompatActivity {
+
+    private ActivityChallengeDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_challenge_details);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_challenge_details);
+
+        //Setting toolbar
+        setSupportActionBar(binding.toolbar);
+
+        // Display icon in the toolbar
+        getSupportActionBar().setTitle("Name of the challenge");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

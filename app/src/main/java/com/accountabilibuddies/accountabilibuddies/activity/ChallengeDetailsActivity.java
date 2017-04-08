c185943a6e2bc20c9cb3c8ecf2e42e6581f90c3b
Divya Yadav <divyayadav1606@gmail.com>
2017-04-08 00:03:22 -0700

    Remove Challenge detail fragment and add a new activity

---
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/ChallengeDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.accountabilibuddies.accountabilibuddies.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.accountabilibuddies.accountabilibuddies.R;
+
+public class ChallengeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_details);
+    }
+}

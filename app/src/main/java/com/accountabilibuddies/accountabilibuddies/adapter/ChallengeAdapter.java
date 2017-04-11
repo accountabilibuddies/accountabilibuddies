1057f0cca422d1e44ee7e31fd81e48060ccb414f
Divya Yadav <divyayadav1606@gmail.com>
2017-04-11 13:53:28 -0700

    error fix

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/ChallengeAdapter.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/ChallengeAdapter.java
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.accountabilibuddies.accountabilibuddies.R;
@@ -28,12 +27,6 @@ public class ChallengeAdapter extends
 
         @BindView(R.id.challengeDescription)
         TextView challengeDescription;
-
-        @BindView(R.id.join)
-        Button join;
-
-        @BindView(R.id.leave)
-        Button leave;
 
         public MyViewHolder(View view) {
             super(view);

541e0a1697a0a95c8416e9741cc30bba7c50b649
Divya Yadav <divyayadav1606@gmail.com>
2017-04-12 00:11:04 -0700

    Add click listeners to like and comment button

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/PostWithImageViewHolder.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/PostWithImageViewHolder.java
@@ -2,6 +2,7 @@ package com.accountabilibuddies.accountabilibuddies.adapter;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.accountabilibuddies.accountabilibuddies.R;
@@ -14,6 +15,12 @@ public class PostWithImageViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.ivPost)
     ImageView imageView;
 
+    @BindView(R.id.ibLike)
+    ImageButton postLike;
+
+    @BindView(R.id.ibComment)
+    ImageButton postComment;
+
     public PostWithImageViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
@@ -25,5 +32,21 @@ public class PostWithImageViewHolder extends RecyclerView.ViewHolder {
 
     public void setImageView(ImageView imageView) {
         this.imageView = imageView;
+    }
+
+    public ImageButton getPostLike() {
+        return postLike;
+    }
+
+    public void setPostLike(ImageButton postLike) {
+        this.postLike = postLike;
+    }
+
+    public ImageButton getPostComment() {
+        return postComment;
+    }
+
+    public void setPostComment(ImageButton postComment) {
+        this.postComment = postComment;
     }
 }
--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/PostWithVideoViewHolder.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/PostWithVideoViewHolder.java
@@ -2,6 +2,7 @@ package com.accountabilibuddies.accountabilibuddies.adapter;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.VideoView;
 
 import com.accountabilibuddies.accountabilibuddies.R;
@@ -14,6 +15,12 @@ public class PostWithVideoViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.video_view)
     VideoView videoView;
 
+    @BindView(R.id.ibLike)
+    ImageButton postLike;
+
+    @BindView(R.id.ibComment)
+    ImageButton postComment;
+
     public PostWithVideoViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
@@ -25,5 +32,21 @@ public class PostWithVideoViewHolder extends RecyclerView.ViewHolder {
 
     public void setVideoView(VideoView videoView) {
         this.videoView = videoView;
+    }
+
+    public ImageButton getPostLike() {
+        return postLike;
+    }
+
+    public void setPostLike(ImageButton postLike) {
+        this.postLike = postLike;
+    }
+
+    public ImageButton getPostComment() {
+        return postComment;
+    }
+
+    public void setPostComment(ImageButton postComment) {
+        this.postComment = postComment;
     }
 }

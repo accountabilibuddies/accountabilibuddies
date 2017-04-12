f5a84d6b0aeb3f7ab9ce28aa56bab1c22e7a554b
Deonna Hodges <dnhodges@gmail.com>
2017-04-12 12:41:35 -0700

    feat(video): Added utility class to play video within a RecyclerView.

---
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/util/VideoPlayer.java
@@ -0,0 +1,56 @@
+package com.accountabilibuddies.accountabilibuddies.util;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.view.View;
+
+import com.yqritc.scalablevideoview.ScalableVideoView;
+
+import java.io.IOException;
+
+public class VideoPlayer {
+
+    /**
+     * Plays video within RecyclerView.
+     *
+     * <p/>
+     *
+     *      Example XML in layout:
+     *     <com.yqritc.scalablevideoview.ScalableVideoView
+     *          android:id="@+id/svvVideo"
+     *          android:layout_width="400dp"
+     *          android:layout_height="200dp"
+     *          android:visibility="gone"
+     *          app:scalableType="fitCenter" />
+     * <p/>
+     *
+     * <pre>
+     *   public void playVideo() {
+     *      VideoPlayer.loadVideo(context, holder.svvVideo, "http://techslides.com/demos/sample-videos/small.mp4");
+     *   }
+     * </pre>
+     *
+     * @param context
+     *          The active {@link Context} for your application.
+     * @param view
+     *          The ScalableVideoView view.
+     * @param url of the video
+     *          The client key provided in the Parse dashboard.
+     */
+    public static void loadVideo(Context context, ScalableVideoView view, String url) {
+
+        try {
+            view.setVisibility(View.VISIBLE);
+            view.setDataSource(context, Uri.parse(url));
+            view.prepareAsync(
+                (MediaPlayer mp) -> {
+                    view.setLooping(true);
+                    view.start();
+                }
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

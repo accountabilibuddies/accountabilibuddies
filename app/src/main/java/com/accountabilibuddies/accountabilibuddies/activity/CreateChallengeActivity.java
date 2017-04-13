24be4f1ba55b325b9a43722aec7fbfe002f6cfe8
divyayadav1606@gmail.com <adi1guddi2>
2017-04-13 01:39:51 -0700

    Create challenge rough layout

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/CreateChallengeActivity.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/CreateChallengeActivity.java
@@ -3,6 +3,8 @@ package com.accountabilibuddies.accountabilibuddies.activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.accountabilibuddies.accountabilibuddies.R;
 import com.accountabilibuddies.accountabilibuddies.databinding.ActivityCreateChallengeBinding;
@@ -16,6 +18,27 @@ public class CreateChallengeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_challenge);
 
-        //Set tool bar with a cross button to move back and a done button to save
+        setSupportActionBar(binding.toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.close));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.create_menu, menu);
+        return true;
     }
 }

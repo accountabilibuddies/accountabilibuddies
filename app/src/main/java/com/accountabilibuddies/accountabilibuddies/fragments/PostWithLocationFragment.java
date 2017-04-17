5bf5d3b248cf0976c955dd61792804a3bac7df76
Deonna Hodges <dnhodges@gmail.com>
2017-04-16 21:01:43 -0700

    feat(post-details): Created fragments for different types of posts.

---
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/fragments/PostWithLocationFragment.java
@@ -0,0 +1,33 @@
+package com.accountabilibuddies.accountabilibuddies.fragments;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.accountabilibuddies.accountabilibuddies.R;
+import com.accountabilibuddies.accountabilibuddies.databinding.ItemPostLocationBinding;
+
+public class PostWithLocationFragment extends Fragment {
+
+    ItemPostLocationBinding binding;
+
+    public static PostWithLocationFragment newInstance() {
+
+        PostWithLocationFragment fragment = new PostWithLocationFragment();
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.item_post_location, parent, false);
+
+        return binding.getRoot();
+    }
+}
---
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/fragments/PostWithVideoFragment.java
@@ -0,0 +1,33 @@
+package com.accountabilibuddies.accountabilibuddies.fragments;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.accountabilibuddies.accountabilibuddies.R;
+import com.accountabilibuddies.accountabilibuddies.databinding.ItemPostVideoBinding;
+
+public class PostWithVideoFragment extends Fragment {
+
+    ItemPostVideoBinding binding;
+
+    public static PostWithVideoFragment newInstance() {
+
+        PostWithVideoFragment fragment = new PostWithVideoFragment();
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.item_post_video, parent, false);
+
+        return binding.getRoot();
+    }
+}

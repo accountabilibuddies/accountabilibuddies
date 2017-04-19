b82d179a588f07c79d5ce73417b9b49003c75740
amodsamant <samant.amod@gmail.com>
2017-04-19 01:33:34 -0700

    Initial changes for create challenge update

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/CreateChallengeActivity.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/CreateChallengeActivity.java
@@ -1,6 +1,7 @@
 package com.accountabilibuddies.accountabilibuddies.activity;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
@@ -16,11 +17,12 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.accountabilibuddies.accountabilibuddies.R;
@@ -32,7 +34,6 @@ import com.accountabilibuddies.accountabilibuddies.network.APIClient;
 import com.accountabilibuddies.accountabilibuddies.util.CameraUtils;
 import com.accountabilibuddies.accountabilibuddies.util.Constants;
 import com.accountabilibuddies.accountabilibuddies.util.DateUtils;
-import com.borax12.materialdaterangepicker.date.DatePickerDialog;
 import com.parse.ParseCloud;
 import com.parse.ParseUser;
 
@@ -42,18 +43,19 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.TimeZone;
 
-public class CreateChallengeActivity extends AppCompatActivity implements
-        DatePickerDialog.OnDateSetListener {
+public class CreateChallengeActivity extends AppCompatActivity {
 
     private ActivityCreateChallengeBinding binding;
     private static int CHALLENGE_TYPE = Constants.TYPE_ONE_ON_ONE;
-    private static int CHALLENGE_FREQUENCY = Constants.FREQUENCY_DAILY;
     private String mImagePath, profileUrl;
     private static final int PHOTO_INTENT_REQUEST = 100;
     private static final int REQUEST_CAMERA = 1;
 
     private AddFriendsFragment addFriendsFragment;
+
+    private static int freq = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,43 +69,12 @@ public class CreateChallengeActivity extends AppCompatActivity implements
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.close));
 
-        setupDateTime();
-        SetupChallengeType();
-        setupFrequency();
-        setUpFriendsView();
-
+        setupViews();
     }
 
-    private void setupFrequency() {
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter
-                .createFromResource(this, R.array.frequency_type,
-                        android.R.layout.simple_spinner_item);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        binding.spFrequency.setAdapter(adapter);
-        binding.spFrequency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        CHALLENGE_FREQUENCY = Constants.FREQUENCY_DAILY;
-                        break;
-                    case 1:
-                        CHALLENGE_FREQUENCY = Constants.FREQUENCY_WEEKLY;
-                        break;
-                    case 2:
-                        CHALLENGE_FREQUENCY = Constants.FREQUENCY_TWICE_A_MONTH;
-                        break;
-                    case 3:
-                        CHALLENGE_FREQUENCY = Constants.FREQUENCY_MONTHLY;
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
+    private void setupViews() {
+        binding.tvFrequency.setText(Constants.frequencyMap.get(freq));
+        setUpFriendsView();
     }
 
     private void setUpFriendsView() {
@@ -113,39 +84,6 @@ public class CreateChallengeActivity extends AppCompatActivity implements
         addFriendsFragment = AddFriendsFragment.newInstance(null);
         ft.replace(R.id.flAddFriends, addFriendsFragment);
         ft.commit();
-    }
-
-    private void SetupChallengeType() {
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter
-                .createFromResource(this, R.array.challenge_type,
-                        android.R.layout.simple_spinner_item);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        binding.spType.setAdapter(adapter);
-        binding.spType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        CHALLENGE_TYPE = Constants.TYPE_ONE_ON_ONE;
-                        break;
-                    case 1:
-                        CHALLENGE_TYPE = Constants.TYPE_MULTI_USER;
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
-    }
-
-    private void setupDateTime() {
-        Calendar calendar = Calendar.getInstance();
-        binding.tvStartDate.setText(DateUtils.getDate(calendar));
-        calendar.add(Calendar.DAY_OF_YEAR, 1);
-        binding.tvEndDate.setText(DateUtils.getDate(calendar));
     }
 
     @Override
@@ -163,109 +101,12 @@ public class CreateChallengeActivity extends AppCompatActivity implements
         return super.onOptionsItemSelected(item);
     }
 
-    private void createChallenge() {
-
-        if (!dataValidation())
-            return;
-
-        Challenge challenge = new Challenge(CHALLENGE_TYPE, binding.etTitle.getText().toString(),
-                                        binding.etDescription.getText().toString(),
-                                        new Date(String.valueOf(binding.tvStartDate.getText())),
-                                        new Date(String.valueOf(binding.tvEndDate.getText())),
-                                        CHALLENGE_FREQUENCY, profileUrl, 0,
-                                        addFriendsFragment.getSelectedFriends());//There is no category so pass 0
-
-        APIClient.getClient().createChallenge(challenge, new APIClient.ChallengeListener() {
-            @Override
-            public void onSuccess() {
-                Intent intent;
-                if(challenge.getType()== Constants.TYPE_ONE_ON_ONE) {
-                    intent = new Intent(CreateChallengeActivity.this, ChallengeOneOnOneActivity.class);
-                } else {
-                    intent = new Intent(CreateChallengeActivity.this, ChallengeDetailsActivity.class);
-                }
-                intent.putExtra("challengeId", challenge.getObjectId());
-                intent.putExtra("name", challenge.getName());
-
-                String currUser = (String) ParseApplication.getCurrentUser().get("name");
-                List<ParseUser> friends = challenge.getUserList();
-
-                for(ParseUser friend : friends) {
-                    if(!friend.get("name").equals(currUser)) {
-                        createChallengeNotification((String)friend.get("name"),
-                                currUser, challenge.getObjectId(), challenge.getType());
-                    }
-                }
-                startActivity(intent);
-                finish();
-            }
-
-            @Override
-            public void onFailure(String error_message) {
-                Log.d("DEBUG", "Failure in creating challenge");
-            }
-        });
-    }
-
-    private boolean dataValidation() {
-
-        if (binding.etTitle.getText().toString().isEmpty()) {
-            Snackbar.make(binding.cLayout, "Title is empty", Snackbar.LENGTH_LONG).show();
-            return false;
-        }
-
-        if (binding.etDescription.getText().toString().isEmpty()) {
-            Snackbar.make(binding.cLayout, "Description is empty", Snackbar.LENGTH_LONG).show();
-            return false;
-        }
-
-        Date startDate = new Date(String.valueOf(binding.tvStartDate.getText()));
-        Date endDate = new Date(String.valueOf(binding.tvEndDate.getText()));
-        Date today = new Date();
-
-        if (!DateUtils.getDateFromDate(today).equals(DateUtils.getDateFromDate(startDate)))  {
-            Snackbar.make(binding.cLayout, "Put a valid start date", Snackbar.LENGTH_LONG).show();
-            return false;
-        }
-
-        if (startDate.compareTo(endDate) > 0 )  {
-            Snackbar.make(binding.cLayout, "End Date should be after start date",
-                    Snackbar.LENGTH_LONG).show();
-            return false;
-        }
-        // if no challenge image use default image
-
-        return true;
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.create_menu, menu);
         return true;
     }
 
-    public void onDateClick(View view) {
-        Calendar now = Calendar.getInstance();
-        DatePickerDialog dpd = com.borax12.materialdaterangepicker.date.DatePickerDialog.newInstance(
-                CreateChallengeActivity.this,
-                now.get(Calendar.YEAR),
-                now.get(Calendar.MONTH),
-                now.get(Calendar.DAY_OF_MONTH)
-        );
-        dpd.setMinDate(now);
-        dpd.show(getFragmentManager(), "Select Date Range");
-    }
-
-    @Override
-    public void onDateSet(DatePickerDialog view, int year, int monthOfYear,
-                          int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd) {
-        Calendar c = Calendar.getInstance();
-        c.set(year, monthOfYear, dayOfMonth, 0, 0);
-        binding.tvStartDate.setText(DateUtils.getDate(c));
-
-        c.set(yearEnd, monthOfYearEnd, dayOfMonthEnd, 0, 0);
-        binding.tvEndDate.setText(DateUtils.getDate(c));
-    }
 
     public void launchCamera(View view) {
         if (CameraUtils.cameraPermissionsGranted(this)) {
@@ -299,36 +140,36 @@ public class CreateChallengeActivity extends AppCompatActivity implements
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        binding.progressBarContainer.setVisibility(View.VISIBLE);
-        binding.avi.show();
+//        binding.progressBarContainer.setVisibility(View.VISIBLE);
+//        binding.avi.show();
 
         switch (requestCode) {
             case PHOTO_INTENT_REQUEST:
                 if (resultCode == RESULT_OK) {
                     if (mImagePath == null) {
-                        binding.avi.hide();
-                        binding.progressBarContainer.setVisibility(View.GONE);
+//                        binding.avi.hide();
+//                        binding.progressBarContainer.setVisibility(View.GONE);
                         return;
                     }
                     Bitmap bitmap = BitmapFactory.decodeFile(mImagePath);
 
-                        APIClient.getClient().uploadFile("post_image.jpg",
-                                    bitmap,new APIClient.UploadFileListener() {
-                        @Override
-                        public void onSuccess(String fileLocation) {
-                            profileUrl = fileLocation;
-                            //Show image in the ImageView
-                            binding.ivProfile.setImageBitmap(bitmap);
-                            binding.avi.hide();
-                            binding.progressBarContainer.setVisibility(View.GONE);
-                        }
+                    APIClient.getClient().uploadFile("post_image.jpg",
+                            bitmap,new APIClient.UploadFileListener() {
+                                @Override
+                                public void onSuccess(String fileLocation) {
+                                    profileUrl = fileLocation;
+                                    //Show image in the ImageView
+//                            binding.ivProfile.setImageBitmap(bitmap);
+//                            binding.avi.hide();
+//                            binding.progressBarContainer.setVisibility(View.GONE);
+                                }
 
-                        @Override
-                        public void onFailure(String error_message) {
-                            binding.avi.hide();
-                            binding.progressBarContainer.setVisibility(View.GONE);
-                        }
-                    });
+                                @Override
+                                public void onFailure(String error_message) {
+//                            binding.avi.hide();
+//                            binding.progressBarContainer.setVisibility(View.GONE);
+                                }
+                            });
                 }
         }
     }
@@ -366,4 +207,142 @@ public class CreateChallengeActivity extends AppCompatActivity implements
             clickChallengePictureIntent(intent);
         }
     }
+
+    private void createChallenge() {
+
+        if (!dataValidation())
+            return;
+
+        Challenge challenge = new Challenge(CHALLENGE_TYPE, binding.etTitle.getText().toString(),
+                binding.etDescription.getText().toString(),
+                new Date(String.valueOf(binding.tvStartDate.getText())),
+                new Date(String.valueOf(binding.tvEndDate.getText())),
+                freq, profileUrl, 0,
+                addFriendsFragment.getSelectedFriends());//There is no category so pass 0
+
+        APIClient.getClient().createChallenge(challenge, new APIClient.ChallengeListener() {
+            @Override
+            public void onSuccess() {
+                Intent intent;
+                if(challenge.getType()== Constants.TYPE_ONE_ON_ONE) {
+                    intent = new Intent(CreateChallengeActivity.this, ChallengeOneOnOneActivity.class);
+                } else {
+                    intent = new Intent(CreateChallengeActivity.this, ChallengeDetailsActivity.class);
+                }
+                intent.putExtra("challengeId", challenge.getObjectId());
+                intent.putExtra("name", challenge.getName());
+
+                String currUser = (String) ParseApplication.getCurrentUser().get("name");
+                List<ParseUser> friends = challenge.getUserList();
+
+                for(ParseUser friend : friends) {
+                    if(!friend.get("name").equals(currUser)) {
+                        createChallengeNotification((String)friend.get("name"),
+                                currUser, challenge.getObjectId(), challenge.getType());
+                    }
+                }
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onFailure(String error_message) {
+                Log.d("DEBUG", "Failure in creating challenge");
+            }
+        });
+    }
+
+    private boolean dataValidation() {
+
+        if (binding.etTitle.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Title is empty", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (binding.etDescription.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Description is empty", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return false;
+    }
+
+
+    public void setStartDate(View view) {
+        Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+        // Create a date picker dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                R.style.AppTheme, datePickerListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.setCancelable(true);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+        Window window = datePickerDialog.getWindow();
+        window.setLayout(WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        datePickerDialog.show();
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener =
+            (view, year, month, dayOfMonth) -> {
+                String startDate = DateUtils
+                        .createSelectedDateString(year, dayOfMonth, month);
+                binding.tvStartDate.setText(startDate);
+            };
+
+    public void setEndDate(View view) {
+
+        if(binding.tvStartDate.getText().length() == 0) {
+            Toast.makeText(view.getContext(),
+                    "Please select start date first!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                R.style.AppTheme, endDatePickerListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.setCancelable(true);
+
+        cal.add(Calendar.DAY_OF_YEAR, 1);
+        datePickerDialog.getDatePicker().setMinDate(cal.getTimeInMillis());
+        Window window = datePickerDialog.getWindow();
+        window.setLayout(WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        datePickerDialog.show();
+    };
+
+    private DatePickerDialog.OnDateSetListener endDatePickerListener =
+            (view, year, month, dayOfMonth) -> {
+                String endDate = DateUtils
+                        .createSelectedDateString(year, dayOfMonth, month);
+                binding.tvEndDate.setText(endDate);
+            };
+
+
+    public void incFrequency(View v) {
+        if(freq>=Constants.frequencyMap.size()) {
+            return;
+        } else {
+            freq++;
+            binding.tvFrequency.setText(Constants.frequencyMap.get(freq));
+        }
+    }
+
+    public void decFrequency(View v) {
+        if(freq<=1) {
+            return;
+        } else {
+            freq--;
+            binding.tvFrequency.setText(Constants.frequencyMap.get(freq));
+        }
+    }
 }
+
--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/util/Constants.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/util/Constants.java
@@ -1,26 +1,24 @@
 package com.accountabilibuddies.accountabilibuddies.util;
 
 import java.util.HashMap;
-import java.util.Map;
 
 abstract public class Constants {
-    // Challenge types, MVP will use only one type of challenge
+
     public static final int TYPE_ONE_ON_ONE = 1;
     public static final int TYPE_MULTI_USER = 2;
-    //public static final int TYPE_TIMELINE = 3;
-
-    // Challenge Frequency,
-    public static final int FREQUENCY_DAILY = 1;
-    public static final int FREQUENCY_WEEKLY = 2;
-    public static final int FREQUENCY_TWICE_A_MONTH = 3;
-    public static final int FREQUENCY_MONTHLY = 4;
-    //public static final int FREQUENCY_FIVE_TIMES = 5;
-    //public static final int FREQUENCY_SIX_TIMES = 6;
-    //public static final int FREQUENCY_ALL_WEEK = 7;
 
     // Post Type
     public static final int TYPE_VIDEO = 1;
     public static final int TYPE_IMAGE = 2;
     public static final int TYPE_TEXT = 3;
     public static final int TYPE_LOCATION = 4;
+
+    public static HashMap<Integer,String> frequencyMap = new HashMap<>();
+    static {
+        frequencyMap.put(1,"Daily");
+        frequencyMap.put(2,"One a week");
+        frequencyMap.put(3,"Twice a week");
+        frequencyMap.put(4,"Once a Month");
+        frequencyMap.put(5,"Twice a Month");
+    }
 }

96351fa2dacfee68b8414d857469b5f930f0cc33
Divya Yadav <divyayadav1606@gmail.com>
2017-04-20 20:01:57 -0700

    comment out notification

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/CreateFriendsAdapter.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/CreateFriendsAdapter.java
@@ -2,14 +2,30 @@ package com.accountabilibuddies.accountabilibuddies.adapter;
 
 import android.content.Context;
 
+import com.accountabilibuddies.accountabilibuddies.R;
 import com.accountabilibuddies.accountabilibuddies.model.Friend;
 import com.bumptech.glide.Glide;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CreateFriendsAdapter extends FriendsAdapter {
+
+    Set<ParseUser> selectedUserList = new HashSet<>();
+
     public CreateFriendsAdapter(Context context, ArrayList<Friend> friendsList) {
         super(context, friendsList);
+    }
+
+    private boolean isFriendAdded(ParseUser friend) {
+        for(ParseUser user : selectedUserList) {
+            if(user.getObjectId().equals(friend.getObjectId())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -20,6 +36,16 @@ public class CreateFriendsAdapter extends FriendsAdapter {
             Glide.with(context)
                     .load(friend.get("profileURL"))
                     .into(holder.profilePic);
+
+            if (!selectedUserList.isEmpty() &&
+                    isFriendAdded(friend.getFriend())) {
+                holder.profilePic.setShadowColor(context.getResources().getColor(R.color.colorAccent));
+            }
         }
+    }
+
+    public void setSelectedList(Set<ParseUser> selectedUserList) {
+        this.selectedUserList = selectedUserList;
+        notifyDataSetChanged();
     }
 }

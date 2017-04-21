3fc123c907c02b0497757c8307c17c4da05d1d73
Divya Yadav <divyayadav1606@gmail.com>
2017-04-21 15:29:07 -0700

    Challenge UI Basic Skeleton

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/ChallengeAdapter.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/adapter/ChallengeAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import com.accountabilibuddies.accountabilibuddies.R;
 import com.accountabilibuddies.accountabilibuddies.model.Challenge;
 import com.bumptech.glide.Glide;
+import com.parse.ParseException;
 
 import java.util.ArrayList;
 
@@ -33,6 +34,12 @@ public class ChallengeAdapter extends
 
         @BindView(R.id.ivChallengeImage)
         ImageView challengeImage;
+
+        @BindView(R.id.ivProfileImage)
+        ImageView profileImage;
+
+        @BindView(R.id.tvMemberCount)
+        TextView count;
 
         public MyViewHolder(View view) {
             super(view);
@@ -58,6 +65,17 @@ public class ChallengeAdapter extends
         Challenge challenge = challengeList.get(position);
 
         if (challenge != null) {
+            holder.challengeImage.setImageResource(0);
+            holder.profileImage.setImageResource(0);
+            try {
+                Glide.with(context)
+                        .load(challenge.getOwner().fetchIfNeeded().getString("profilePhotoUrl"))
+                        .into(holder.profileImage);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            holder.count.setText(String.valueOf(challenge.getUserList().size()));
             holder.challengeName.setText(challenge.getName());
             holder.challengeDescription.setText(challenge.getDescription());
 

5b4322df69db899d88e2a1da782c07fdd2658d90
amodsamant <samant.amod@gmail.com>
2017-04-23 02:17:47 -0700

    Added different date theme so that date dialog is clearly visible

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/CreateChallengeActivity.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/CreateChallengeActivity.java
@@ -360,7 +360,7 @@ public class CreateChallengeActivity extends AppCompatActivity {
     private void showDateDialog(DatePickerDialog.OnDateSetListener listener, long minDate) {
         Calendar cal = Calendar.getInstance(TimeZone.getDefault());
         DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                R.style.AppTheme, listener,
+                R.style.SelectDateTheme, listener,
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH));

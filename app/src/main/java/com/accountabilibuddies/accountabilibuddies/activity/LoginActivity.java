3d3b3f1ce6f1847d975b114530e0941b5df93888
Divya Yadav <divyayadav1606@gmail.com>
2017-04-29 14:14:13 -0700

    Check in temp facebook key hash code

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/LoginActivity.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/LoginActivity.java
@@ -30,7 +30,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+/* Temp code to generate fb key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.accountabilibuddies.accountabilibuddies",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
 
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+*/
         viewModel = new LoginViewModel(LoginActivity.this);
 
         ViewUtils.makeViewFullScreen(getWindow());

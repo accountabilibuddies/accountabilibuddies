2e6f37c3aa3be7bcdc6c597ef210c7f534b3e37c
Divya Yadav <divyayadav1606@gmail.com>
2017-05-07 12:52:58 -0700

    Add splash Activity

--- app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/SplashActivity.java
+++ app/src/main/java/com/accountabilibuddies/accountabilibuddies/activity/SplashActivity.java
@@ -5,16 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.accountabilibuddies.accountabilibuddies.R;
+import com.bumptech.glide.Glide;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class SplashActivity extends AppCompatActivity {
 
-    public static final int DELAY = 2;
+    public static final int DELAY = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Glide.with(this)
+                .load(R.drawable.splash_animation)
+                .asGif()
+                .into((ImageView)findViewById(R.id.gif));
 
         new CountDownTimer(DELAY * 1000, 1000) {
             @Override
